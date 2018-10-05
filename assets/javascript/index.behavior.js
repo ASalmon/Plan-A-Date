873f9a2aaa7a8c9b34189a7159fd4323b6f7edd4
Alton Salmon <asalmon@altons-mbp-2.lan>
2018-10-05 16:16:54 -0400

    added js and image files

--- assets/javascript/index.behavior.js
+++ assets/javascript/index.behavior.js
@@ -1,4 +1,4 @@
-$(document).ready(function( $ ) {
+jQuery(document).ready(function( $ ) {
 
   // Back to top button
   $(window).scroll(function() {
@@ -126,48 +126,4 @@ $(document).ready(function( $ ) {
     },
     allowPageScroll:"vertical"
   });
-
-  // Skills section
-  $('#skills').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  }, { offset: '80%'} );
-
-  // jQuery counterUp (used in Facts section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // Porfolio isotope and filter
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-item',
-    layoutMode: 'fitRows'
-  });
-
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
-
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  });
-
-  // Clients carousel (uses the Owl Carousel library)
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
-    }
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
 });
