fd3c7221a45e30a07a67a0f6b05d2026d51caa38
Alton Salmon <asalmon@Altons-MacBook-Pro-2.local>
2018-10-06 11:17:02 -0400

    updated main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,29 @@
 var eventBriteAPIKey = "4EWBHY45SSIIT4IW5U63";
 
-   // Here we are building the URL we need to query the database
+// Here we are building the URL we need to query the database
+var userInput = 'plays';
+var cityInput = 'Atlanta';
+//var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=&token=" + eventBriteAPIKey;
+var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + "&location.address=" + cityInput + "&token=" + eventBriteAPIKey;
+//samlpe query "https://www.eventbriteapi.com/v3/events/search/?q=" + userInput + "&location.address=" + cityInput + "&token=" + eventBriteAPIKey;;
 
-   var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=&token=" + eventBriteAPIKey;
+// Here we run our AJAX call to the OpenWeatherMap API
+var test;
 
-   //samlpe query "https://www.eventbriteapi.com/v3/events/search/?q=" + userInput + "&location.address=" + cityInput + "&token=" + token;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function (data) {
+      test = data.events;
+      for (var i = 0; i < test.length && i < 10; i++) {
+        var content = "<h2>" + test[i].name.text;
+        $("#results").append(content);  
 
-   // Here we run our AJAX call to the OpenWeatherMap API
-   $.ajax({
-     url: queryURL,
-     method: "GET"
-   })
-     // We store all of the retrieved data inside of an object called "response"
-     .then(function(response) {
-
-       // Log the queryURL
-       console.log(queryURL);
-
-       // Log the resulting object
-       console.log(response);
-
-       // Log the data in the console as well probably need to run loop to loop through list
-       console.log("name: " + response.events[0].name.text);
-       console.log("imageurl: " + response.events[0].logo.original.url);
-
-     });
+      }
+      // test.forEach(function(result) {
+      //   var content = "<h2>" + result.name.text + "</h2>" + result.description.html;
+      //   $("#results").append(content);  
+      // });
+  });
