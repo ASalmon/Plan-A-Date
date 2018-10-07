d12bcbbc047ec2b7fe31bac2e3948fa60e36319a
Alton Salmon <asalmon@Altons-MacBook-Pro-2.local>
2018-10-06 20:24:31 -0400

    updated main.js to display search results

--- assets/javascript/ajax.requests.js
+++ assets/javascript/ajax.requests.js
@@ -1,21 +1,25 @@
-var searchEvent = function (event, location) {
+var searchEvent = function (events, location) {
    var eventBriteAPIKey = "4EWBHY45SSIIT4IW5U63";
-   var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + "&location.address=" + location + "&token=" + eventBriteAPIKey;
+   var queryURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + events + "&location.address=" + location + "&token=" + eventBriteAPIKey;
 
    $.ajax({
       url: queryURL,
       method: "GET"
    }).then(function (response) {
-      // test = data.events;
-      // for (var i = 0; i < test.length && i < 10; i++) {
-      //   var content = "<h2>" + test[i].name.text;
-      //   $("#results").append(content);  
-      // }
+      var individualResultContainer = $('<div>').addClass('col-lg-4 col-md-6 search-item filter-food wow fadeInUp')
+          .append($('<div>').addClass('search-wrap')
+          .append($('<figure>')
+          .append(
+           $('<img>').attr('src', response.image_url)
+          )
+         )
+      )
+      resultsContainer.append(individualResultContainer); 
    });
 };
 
-var searchFood = function(food, loc) {
-   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?text=&term=" + food + "&location=" + loc;  
+var searchFood = function(food, location) {
+   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?text=&term=" + food + "&location=" + location;  
    $.ajax({
        url: queryURL,
        method: 'GET',
@@ -23,6 +27,16 @@ var searchFood = function(food, loc) {
            'authorization' : 'Bearer E7MDSicyKUxKA-QyaBY69yUUE96x1vw9m4aL0o7hHyHieVHcjNA8gkLWE8Th2qLFDskdLjG7F4szURHf_AoH9SxoeVhUTCZWgg-10xXtLcYAnFv5CvGTU-n4rSO0W3Yx',   
        } 
    }).then(function(response) {
+    console.log(response);
+      var individualResultContainer = $('<div>').addClass('col-lg-4 col-md-6 search-item filter-food wow fadeInUp')
+          .append($('<div>').addClass('search-wrap')
+          .append($('<figure>')
+          .append(
+           $('<img>').attr('src', response.business[0].image_url)
+          )
+         )
+      )
+      resultsContainer.append(individualResultContainer); 
 
-   }); 
+   });
 };
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,37 +4,68 @@ var $locationInputElement = $('#location-input');
 var $foodInputElement = $('#food-input');
 var $eventInputElement = $('#event-input');
 var $searchButtonElement = $('#search-button');
-var $searchResultsElement = $('#search');
+var $searchResultsElement = $('#search-result');
 
-$(document).ready(function() {
-  $searchButtonElement.on('click', function(event) {
-    event.preventDefault();
-    // Store user input in a local variable
-    var dateInput = $dateInputElement.val().trim();
-    var locationInput = $locationInputElement.val().trim();
-    var foodInput = $foodInputElement.val().trim();
-    var eventInput = $eventInputElement.val().trim();
-    // Search for events and food using eventbright and yelp    
-    var foodresult = searchFood(foodInput, locationInput);
-    var eventResult = searchEvent(eventInput, locationInput);
 
+//$(document).ready(function () {
+$searchButtonElement.on('click', function (event) {
+  event.preventDefault();
+  // Store user input in a local variable
+  var dateInput = $dateInputElement.val().trim();
+  var locationInput = $locationInputElement.val().trim();
+  var foodInput = $foodInputElement.val().trim();
+  var eventInput = $eventInputElement.val().trim();
+  // Search for events and food using eventbright and yelp    
+  //searchFood(foodInput, locationInput);
+  //searchEvent(eventInput, locationInput);
+
+
+  var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?text=&term=" + foodInput + "&location=" + locationInput;
+  $.ajax({
+    url: queryURL,
+    method: 'GET',
+    headers: {
+      'authorization': 'Bearer E7MDSicyKUxKA-QyaBY69yUUE96x1vw9m4aL0o7hHyHieVHcjNA8gkLWE8Th2qLFDskdLjG7F4szURHf_AoH9SxoeVhUTCZWgg-10xXtLcYAnFv5CvGTU-n4rSO0W3Yx',
+    }
+  }).then(function (response) {
+    console.log(response);
     var resultContainer = $('<div>').addClass('container');
     var resultHeader = $('<header>').addClass('section-header').append(
       //Change the text below if you wish to change search results 
       $('<h3>').addClass('section-title').text('Search Results')
     );
-    resultContainer.append(resultHeader);
 
     var resultTabs = $('<div>').addClass('row').append(
       $('<div>').addClass('col-lg-12').append(
-        $('<ul>').attr('id','search-flters').append(
-          $('<li>').attr('data-filter', 'filter-app').text('Food'),
-          $('<li>').attr('data-filter', 'filter-card').text('Event')
+        $('<ul>').attr('id', 'search-result-flters').append(
+          $('<li>').attr('data-filter', 'filter-food').text('Food'),
+          $('<li>').attr('data-filter', 'filter-events').text('Event')
         )
       )
-    )
-    $searchResultsElement.append(resultContainer,
-      resultTabs  
     );
+
+    var newRow = $('<div>').addClass('row search-result-container');
+
+    var newCol = $('<div>').addClass('col-lg-4 col-md-6 search-result-item filter-food wow fadeInUp');
+
+    var individualresultwrapper = $('<div>').addClass('search-result-wrap');
+
+    var newfigure = $('<figure>');
+
+    var newimg = $('<img>').attr('src', response.businesses[0].image_url).addClass('img-fluid');
+    
+    var resultinfo = $('<div>').addClass('search-result-info').append(
+      $('<h6>').text(response.businesses[0].name)
+    );
+    newfigure.append(newimg);
+
+    individualresultwrapper.append(newfigure, resultinfo);
+    newCol.append(individualresultwrapper);
+    newRow.append(newCol);
+
+    resultContainer.append(resultHeader, resultTabs, newCol);
+    $('#search-result').append(resultContainer);
+
   });
+
 });
