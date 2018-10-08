1835a619b8cb440b1114fcc7a19daf15a6c521ac
Alton Salmon <asalmon@Altons-MacBook-Pro-2.local>
2018-10-07 22:12:32 -0400

    updated main.js to reflect index.html change

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,4 @@
 // DOM Elements Query
-var $dateInputElement = $('#date-input');
 var $locationInputElement = $('#location-input');
 var $foodInputElement = $('#food-input');
 var $eventInputElement = $('#event-input');
@@ -11,7 +10,6 @@ var $searchResultsElement = $('#search-result');
 $searchButtonElement.on('click', function (event) {
   event.preventDefault();
   // Store user input in a local variable
-  var dateInput = $dateInputElement.val().trim();
   var locationInput = $locationInputElement.val().trim();
   var foodInput = $foodInputElement.val().trim();
   var eventInput = $eventInputElement.val().trim();
